cd0a70ae50a34cb5248d8c44c96e570672f67985
Nikola Stojanovic <35263124+generalni@users.noreply.github.com>
2018-05-02 21:57:36 +0200

    jQuery, unos imena, brojanje poteza

--- js/script.js
+++ js/script.js
@@ -1,68 +1,101 @@
-var niz=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-var niz1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]; // ovaj sluzi za poredjenje
-var br=niz.length;
+var niz = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+var nizSredjen = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]; // ovaj niz sluzi za poredjenje
+var brojNeslozenihPolja = niz.length;
+var brojPoteza = 0;
+var imeIgraca;
 
-function izmesaj() {
-	for(var i = niz.length - 1; i > 0; i--)	{
-			var j = Math.floor(Math.random() * (i + 1));
-			tmp = niz[i];
-			niz[i] = niz[j];
-			niz[j] = tmp;
+function napraviTablu() {
+	for (var i = 0; i < niz.length; i++) {
+		if (niz[i] != 0)	{
+			$("#container").append('<div onclick="klik('+i+')" class="polje">'+niz[i]+'</div>');
+		}	else {
+			$("#container").append('<div class="polje-prazno">'+niz[i]+'</div>'); // samo vrednost 0 ima belu pozadinu
+		}
 	}
 }
 
-function gen() {	
-	var brojac=0;
-	var container=document.getElementById("container");
-	for (var i=0; i<niz.length; i++) {	
-		if(niz[i]!=0)	{
-				container.innerHTML+='<div onclick="premesti('+i+')" class="polja">'+niz[i]+'</div>';
-			}	else {
-				container.innerHTML+='<div onclick="premesti('+i+')" class="prazno-polje">'+niz[i]+'</div>'; // samo vrednost 0 ima belu pozadinu
-			}
+function izmesaj() {
+	for (var i = niz.length - 1; i > 0; i--) {
+		var nasumicniBroj = Math.floor(Math.random() * (i + 1));
+		tmp = niz[i];
+		niz[i] = niz[nasumicniBroj];
+		niz[nasumicniBroj] = tmp;
 	}
 }
 
 // cisti div kako bi mogao da prikaze novu tablu
 function cisti() {
-	container.innerHTML=''; 
+	container.innerHTML="";
 }
 
 // premesta polje u prazno polje
-function premesti(a) {
-	if(niz[a-4]==0)	{
-			priv=niz[a-4];
-			niz[a-4]=niz[a];
-			niz[a]=priv;
-		}	else if(niz[a+4]==0) {
-			priv=niz[a+4];
-			niz[a+4]=niz[a];
-			niz[a]=priv;
-		}	else if(niz[a-1]==0) {
-			priv=niz[a-1];
-			niz[a-1]=niz[a];
-			niz[a]=priv;
-		}	else if(niz[a+1]==0) {
-			priv=niz[a+1];
-			niz[a+1]=niz[a];
-			niz[a]=priv;
-		}
+function klik(a) {
+	if (niz[a-4] == 0) {
+		priv = niz[a-4];
+		niz[a-4] = niz[a];
+		niz[a] = priv;
+	}	else if (niz[a+4] == 0) {
+		priv = niz[a+4];
+		niz[a+4] = niz[a];
+		niz[a] = priv;
+	}	else if (niz[a-1] == 0) {
+		priv = niz[a-1];
+		niz[a-1] = niz[a];
+		niz[a] = priv;
+	}	else if (niz[a+1] == 0) {
+		priv = niz[a+1];
+		niz[a+1] = niz[a];
+		niz[a] = priv;
+	}
+	brojPoteza = brojPoteza + 1;
 	cisti();
-	gen();
+	napraviTablu();
 	provera();
 	return(niz);
 }
 
+function unosImena() {
+	imeIgraca = $("#ime").val();
+	if (imeIgraca == "") {
+		alert("Molimo unesite ime");
+	} else {
+		$("#prozor").css("display", "none");
+		$("main").css("display", "flex");
+		napraviTablu();
+		return imeIgraca;
+	}
+}
+
 // poruka koju igrica izbacuje kada su elementi niza poredjani po redu
 function provera() {
-	for(i=0;i<niz.length;i++)	{
-		if(niz[i]==niz1[i])	{	
-			br=br-1;
-			if(br==0)	{
-				alert("USPELI STE! BRAVO!");
+	for (i = 0; i < niz.length; i++)	{
+		if (niz[i] == nizSredjen[i])	{	
+			brojNeslozenihPolja = brojNeslozenihPolja - 1;
+			if (brojNeslozenihPolja == 0)	{
+				$("main").css({
+					"opacity" : "0.3",
+					"pointer-events" : "none"
+				});
+				$("#prozor").css({
+					"display" : "flex",
+					"user-select" : "none",
+					"cursor" : "pointer"
+				});
+				$("#prozor").html("Bravo " + imeIgraca + ", završili ste igru u " + brojPoteza + " poteza <br> (kliknite na ovaj prozor da generišete novu tablu)");
+				$("#prozor").click(function() {
+					$("#prozor").css("display", "none");
+					$("main").css({
+						"opacity" : "1",
+						"pointer-events" : "auto",
+						"cursor" : "pointer"
+					});
+					cisti();
+					izmesaj();
+					napraviTablu();
+				});
 			}
 		}	else {
-			br=niz.length;
+			brojNeslozenihPolja = niz.length;
 		}
 	}
 }
